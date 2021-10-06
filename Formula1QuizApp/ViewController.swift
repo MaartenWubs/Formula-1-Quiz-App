c1e6bd550b0c5e969e6adb4861f46d3033d29bc8
Maarten Wubs <maarten.wubs@icloud.com>
2021-10-06 14:21:18 +0200

    Initial Commit

---
+++ Formula1QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Formula1QuizApp
+//
+//  Created by Maarten Wubs on 10/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
